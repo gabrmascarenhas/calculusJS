2a2742ccd8c9c9e3ca9c3139a29386769470d428
gabrmascarenhas <gdmoura412@gmail.com>
2025-05-16 17:34:09 -0300

    Starting another one.

---
+++ Mediumvaluefunction.js
@@ -0,0 +1,17 @@
+//Código para calcular o valor médio de uma Função f(x):
+
+//Parte 1: Valores médios:
+//     Declaração da Integral da velocidade:
+
+let f = function(x) { 
+    return -0.0000000136591 * (x**9) +
+     0.0000017062351 * (x**8) - 
+     0.0000899034328 * (x**7) +
+     0.0026075494886 * (x**6) -
+     0.0455191990079 * (x**5) +
+     0.4896600639736 * (x**4) -
+     3.1645461679853 * (x**3) +
+     11.1509652193998 * (x**2) -
+     14.7353973494401 * x;
+
+}
