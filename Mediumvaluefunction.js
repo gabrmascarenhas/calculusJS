27a62fe3d8d2b0746e5e4346e4fdab2c07ea85a9
gabrmascarenhas <gdmoura412@gmail.com>
2025-05-17 15:56:14 -0300

    tentativa 1

--- Mediumvaluefunction.js
+++ Mediumvaluefunction.js
@@ -19,7 +19,7 @@ let f = function(x) {
  //    Declaração da Integral da aceleração: (velocidade)
 
 
- let F = function(x) {
+ let F = function(t) {
     return -0.0000001229316 * (t**8) + 
     0.0000136498812 * (t**7) - 
     0.0006293240298 * (t**6) +  
@@ -30,3 +30,17 @@ let f = function(x) {
     22.3019304387997 * t - 
     10.8353973494401;
  }
+
+ //Declaração do começo e final da função total
+ let a = 0; //Início
+ let b = 32; //Final
+ var valormedio = 0;
+ 
+ 
+// Ao integrar uma função por (b) até (a) podemos reescrever como (f(b) + C) - (f(a) + C) aonde cancelamos as duas constantes de integração deixando apenas:
+//    f(b) - f(a)
+console.log("Valor médio da aceleração:");
+console.log((F(b) - F(a)) / (b - a));
+
+console.log("Valor médio da velocidade:");
+console.log((f(b) - f(a)) / (b - a));
